9ac2c4281a3a24727a7d193bd6769fb188a4cb75
Valery <valeriagruzilova@yandex.ru>
2021-03-14 16:52:43 +0300

    Creating display

---
+++ MagicRush.py
@@ -0,0 +1,27 @@
+import pygame
+
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+display = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('Magic Rush')
+
+icon = pygame.image.load('background/icon.jpg')
+pygame.display.set_icon(icon)
+
+def run_game():
+    game = True
+
+    while game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        display.fill((255, 255, 255))
+        pygame.display.update()
+
+
+run_game()
